bbbfd5a79d0caae49cb17f85fad39ffc5e80b9e6
SallyMulupi <mulupisally2gmail.com>
2022-07-15 23:42:14 +0300

    add const food and usestate

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -2,5 +2,6 @@ import React, { useState } from "react";
 import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
+    const [foods, setFoods] = useState(spicyFoods);
 
     export default SpicyFoodList;
